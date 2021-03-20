9698171b5d3b8bec4968f15aa1e6cb398ed05edd
Tina G <tina@smartlogic.io>
2021-03-20 19:33:48 -0400

    ⚙️ Config: Graphql, apollo, codegen
    Adds graphql, apollo, and codegen dependencies for the app. Wraps the
    app in an apollo provider.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 
 import theme from '../theme';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  credentials: "include",
+  cache: new InMemoryCache()
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
